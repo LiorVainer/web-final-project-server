256a714480d9b277ffc7b822466701eb3a4d96bc
Lior Vainer <liorvainer@gmail.com>
2025-02-26 14:50:08 +0200

    Fixed Script Dev & Start by replacing bcrypt by bcryptjs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-import { initApp } from "./server";
+import {initApp} from "./server";
 
 const port = process.env.PORT;
 
-try {
-  const app = await initApp();
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-} catch (error) {
-  console.error("Failed to initialize the app:", error);
+const main = async () => {
+    try {
+        const app = await initApp();
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error("Failed to initialize the app:", error);
+    }
 }
+
+main();
